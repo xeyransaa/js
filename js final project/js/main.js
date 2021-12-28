fa0dc1243ba5aec46833a689cb44400e796b6c24
xeyransaa <78117146+xeyransaa@users.noreply.github.com>
2021-12-28 08:26:35 +0400

    Add files via upload

---
+++ js final project/js/main.js
@@ -0,0 +1,8 @@
+var sliderHeight = document.querySelector(".head-slider").clientHeight * document.querySelectorAll(".head-slider .slides .slide").length + "px"
+document.querySelector(".head-slider .slides").style.height=sliderHeight
+
+var i = 0;
+// function slider(){
+//     i++;
+//     document.querySelector(".head-slider slides").style.top = -()
+// }
